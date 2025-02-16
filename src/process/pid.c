7d620025e99faeb189b0e333465fff254c7e97ea
nescapp <97590612+nescapp@users.noreply.github.com>
2025-02-16 22:44:02 +0100

    formatted files

--- src/process/pid.c
+++ src/process/pid.c
@@ -4,21 +4,21 @@
 #include <signal.h>
 
 bool process_pid_is_alive(ProcessPid pid) {
-  int32_t res = kill(pid, 0);
-  if (res == 0) {
-    return true;
-  }
-  if (res == -1) {
-    switch (errno) {
-    case ESRCH:
-      return false;
-    case EPERM:
-      return true;
-    default:
-      perror("Unknown result checking if PID is alive");
-      return false;
+    int32_t res = kill(pid, 0);
+    if(res == 0) {
+        return true;
     }
-  }
-  fprintf(stderr, "Reached an unreachable code section\n");
-  abort();
+    if(res == -1) {
+        switch(errno) {
+        case ESRCH:
+            return false;
+        case EPERM:
+            return true;
+        default:
+            perror("Unknown result checking if PID is alive");
+            return false;
+        }
+    }
+    fprintf(stderr, "Reached an unreachable code section\n");
+    abort();
 }
--- src/thread/thread.c
+++ src/thread/thread.c
@@ -13,120 +13,122 @@
 
 // Linux: pthreads
 struct Thread {
-  pthread_t tid;
-  ThreadCallback callback;
-  void *context;
+    pthread_t tid;
+    ThreadCallback callback;
+    void* context;
 };
 
-static void *thread_body(void *context) {
-  pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL);
-  pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
-  Thread *thread = context;
-  return thread->callback(thread->context);
+static void* thread_body(void* context) {
+    pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL);
+    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+    Thread* thread = context;
+    return thread->callback(thread->context);
 }
 
-Thread *thread_start(ThreadCallback callback, void *context) {
-  Thread *thread = malloc(sizeof(Thread));
-  thread->callback = callback;
-  thread->context = context;
-  int32_t res = pthread_create(&thread->tid, NULL, thread_body, thread);
-  if (res != 0) {
-    errno = res;
-    perror("pthread_create()");
-    free(thread);
-    return NULL;
-  }
-  return thread;
+Thread* thread_start(ThreadCallback callback, void* context) {
+    Thread* thread = malloc(sizeof(Thread));
+    thread->callback = callback;
+    thread->context = context;
+    int32_t res = pthread_create(&thread->tid, NULL, thread_body, thread);
+    if(res != 0) {
+        errno = res;
+        perror("pthread_create()");
+        free(thread);
+        return NULL;
+    }
+    return thread;
 }
 
-void thread_cancel(Thread *thread) {
-  int32_t res = pthread_cancel(thread->tid);
-  if (res != 0) {
-    errno = res;
-    perror("pthread_cancel()");
-  }
+void thread_cancel(Thread* thread) {
+    int32_t res = pthread_cancel(thread->tid);
+    if(res != 0) {
+        errno = res;
+        perror("pthread_cancel()");
+    }
 }
 
-bool thread_try_join(Thread *thread, void **result) {
-  void *ret;
-  int32_t res;
+bool thread_try_join(Thread* thread, void** result) {
+    void* ret;
+    int32_t res;
 
 #ifdef __APPLE__
-  struct timespec ts;
-  clock_gettime(CLOCK_REALTIME, &ts);
-  ts.tv_nsec += 1000000; // Set a very short timeout (1 millisecond)
-  if (ts.tv_nsec >= 1000000000) {
-    ts.tv_sec += 1;
-    ts.tv_nsec -= 1000000000;
-  }
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    ts.tv_nsec += 1000000; // Set a very short timeout (1 millisecond)
+    if(ts.tv_nsec >= 1000000000) {
+        ts.tv_sec += 1;
+        ts.tv_nsec -= 1000000000;
+    }
 
-  pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-  pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-  pthread_mutex_lock(&mutex);
-  res = pthread_cond_timedwait(&cond, &mutex, &ts);
-  pthread_mutex_unlock(&mutex);
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+    pthread_mutex_lock(&mutex);
+    res = pthread_cond_timedwait(&cond, &mutex, &ts);
+    pthread_mutex_unlock(&mutex);
 
-  if (res == ETIMEDOUT) {
-    res = EBUSY;
-  } else {
-    res = pthread_join(thread->tid, &ret);
-  }
+    if(res == ETIMEDOUT) {
+        res = EBUSY;
+    } else {
+        res = pthread_join(thread->tid, &ret);
+    }
 #else
-  res = pthread_tryjoin_np(thread->tid, &ret);
+    res = pthread_tryjoin_np(thread->tid, &ret);
 #endif
 
-  if (res != 0) {
-    if (res == EBUSY) {
-      return false;
+    if(res != 0) {
+        if(res == EBUSY) {
+            return false;
+        }
+        errno = res;
+        perror("pthread_tryjoin_np()");
+        return false;
     }
-    errno = res;
-    perror("pthread_tryjoin_np()");
-    return false;
-  }
-  if (ret == PTHREAD_CANCELED) {
-    ret = NULL;
-  }
-  if (result != NULL) {
-    *result = ret;
-  }
-  free(thread);
-  return true;
+    if(ret == PTHREAD_CANCELED) {
+        ret = NULL;
+    }
+    if(result != NULL) {
+        *result = ret;
+    }
+    free(thread);
+    return true;
 }
 
-void thread_join(Thread *thread, void **result) {
-  void *ret;
-  int32_t res = pthread_join(thread->tid, &ret);
-  if (res != 0) {
-    errno = res;
-    perror("pthread_join()");
-    *result = NULL;
+void thread_join(Thread* thread, void** result) {
+    void* ret;
+    int32_t res = pthread_join(thread->tid, &ret);
+    if(res != 0) {
+        errno = res;
+        perror("pthread_join()");
+        *result = NULL;
+        return;
+    }
+    if(ret == PTHREAD_CANCELED) {
+        ret = NULL;
+    }
+    if(result != NULL) {
+        *result = ret;
+    }
+    free(thread);
     return;
-  }
-  if (ret == PTHREAD_CANCELED) {
-    ret = NULL;
-  }
-  if (result != NULL) {
-    *result = ret;
-  }
-  free(thread);
-  return;
 }
 
 void thread_self_enable_canceling() {
-  pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
 }
 
 void thread_self_disable_canceling() {
-  pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
 }
 
 void thread_self_quit_if_canceled() {
-  int32_t old_state;
-  pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, &old_state);
-  pthread_testcancel();
-  if (old_state == PTHREAD_CANCEL_DISABLE) {
-    pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
-  }
+    int32_t old_state;
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, &old_state);
+    pthread_testcancel();
+    if(old_state == PTHREAD_CANCEL_DISABLE) {
+        pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+    }
 }
 
-void thread_self_usleep(useconds_t usec) { usleep(usec); }
+void thread_self_usleep(useconds_t usec) {
+    usleep(usec);
+}
